74d93b521338d3ade0480ed04802f789ace1b53e
Manasvi070902 <manasvi.alimchandani2019@vitstudent.ac.in>
2021-12-06 03:10:33 +0530

    files upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express"),
+ session = require("express-session"),
+ mongoose = require("mongoose"),
+ cookieParser = require('cookie-parser'),
+ routes = require("./routes"),
+ { passport } = require('./passport'),
+dotenv = require('dotenv')
+ dotenv.config()
+ PORT = process.env.PORT || 3001,
+ app = express();
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+// Passport & Session
+app.use(session({ secret: "Nincompoop", resave: true, saveUninitialized: true }));
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Serve static assets
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI )
+    .then(console.log("Connection to database established"));
+
+// Routes
+app.use(routes);
+
+// Start server
+app.listen(PORT, function() {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
